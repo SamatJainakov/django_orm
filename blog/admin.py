0873601e88de08f93ccc8800cfdc44b4cfae6915
SamatJainakov <jsamatbars@gmail.com>
2023-04-15 18:23:24 +0600

    'second'

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Publication, Article, Author
 
-# Register your models here.
+
+admin.site.register(Publication)
+admin.site.register(Author)
+admin.site.register(Article)
